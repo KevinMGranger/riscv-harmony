ccabad0ebd5627bbd631a891cb12df128179e01a
Kevin M Granger <git@kevinmgranger.me>
2016-11-20 17:34:25 -0500

    Add more memory wrk

--- src/memory.rs
+++ src/memory.rs
@@ -1,10 +1,9 @@
 use std::collections::HashMap;
-use std::cell::RefCell;
 
-const slabsize: u32 = 256; // 1 KiB == 256 32-bit words
+const slabsize: u32 = 1024; // 1 KiB == 256 32-bit words
 
 pub struct Memory {
-    slabs: HashMap<(u32, u32), Vec<u32>>
+    slabs: HashMap<(u32, u32), Vec<u8>>
 }
 
 fn range_for(index: u32) -> ((u32, u32), u32) {
@@ -21,17 +20,68 @@ impl Memory {
         }
     }
 
-    pub fn get(&self, index: u32) -> u32 {
+    pub fn get_byte(&self, index: u32) -> Option<u8> {
         let (range, diff) = range_for(index);
-        match self.slabs.get(&range) {
-            Some(x) => x[diff as usize],
-            None => 0
+        self.slabs.get(&range).map(|vec| vec[diff as usize])
+    }
+
+    pub fn get_half(&self, index: u32) -> Option<u16> {
+        let mut val = 0u16;
+
+        for i in 0..2 {
+            if let Some(x) = self.get_byte(index + i) {
+                val = (x as u16) << i;
+            } else {
+                return None
+            }
+        }
+
+        Some(val)
+    }
+
+    pub fn get_word(&self, index: u32) -> Option<u32> {
+        let mut val = 0u32;
+
+        for i in 0..4 {
+            if let Some(x) = self.get_byte(index + i) {
+                val = (x as u32) << i;
+            } else {
+                return None
+            }
+        }
+
+        Some(val)
+    }
+
+    pub fn set_byte(&mut self, index: u32, value: u8) {
+        let (range, diff) = range_for(index);
+        let mut vec = self.slabs.entry(range).or_insert_with(|| vec![0u8; slabsize as usize]);
+        vec[diff as usize] = value
+    }
+
+    pub fn set_half(&mut self, index: u32, value: u16) {
+        for i in 0..2 {
+            let byte = (value >> i) as u8;
+            self.set_byte(index + i, byte);
         }
     }
 
-    pub fn set(&mut self, index: u32, value: u32) {
-        let (range, diff) = range_for(index);
-        let vec = self.slabs.entry(range).or_insert_with(|| vec![0; slabsize as usize]);
-        vec[diff as usize] = value
+    pub fn set_word(&mut self, index: u32, value: u32) {
+        for i in 0..4 {
+            let byte = (value >> i) as u8;
+            self.set_byte(index + i, byte);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn store_get_byte() {
+        let mut mem = Memory::new();
+        mem.set_byte(0, 1);
+        assert_eq!(mem.get_byte(0), Some(1))
     }
 }
